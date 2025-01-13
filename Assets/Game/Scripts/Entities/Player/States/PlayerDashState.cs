f76d79f0136823951d8d65627c79b285b8c0dbbb
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2025-01-13 19:37:39 +0100

    feat(player): dash animation

--- Assets/Game/Scripts/Entities/Player/States/PlayerDashState.cs
+++ Assets/Game/Scripts/Entities/Player/States/PlayerDashState.cs
@@ -1,6 +1,7 @@
 using Databases;
 using Game.Systems.StateMachine;
 using LemonInc.Core.Utilities;
+using UnityEngine;
 
 namespace Game.Entities.Player.States
 {
@@ -8,9 +9,13 @@ namespace Game.Entities.Player.States
     {
         private IInputProvider _input;
         private Controller _controller;
-        
+        private Animator _animator;
+
+        private static readonly int Dash = Animator.StringToHash("Dash");
+
         public override void Awake()
         {
+            _animator = StateMachine.Owner.GetComponentInChildren<Animator>();
             _input = StateMachine.Owner.GetComponent<IInputProvider>();
             _controller = StateMachine.Owner.GetComponent<Controller>();
         }
@@ -24,8 +29,8 @@ namespace Game.Entities.Player.States
             _controller.SetSpeed(speed);
             _controller.SetDirection(_input.AimDirection);
             _controller.LockAim(true, _input.AimDirection);
-            Awaiter.WaitAndExecute(.2f, () => StateMachine.SwitchState(Payload.Idle));
-            
+            _animator.SetTrigger(Dash);
+            Awaiter.WaitAndExecute(time, () => StateMachine.SwitchState(Payload.Idle));
         }
         
         public override void Exit()
