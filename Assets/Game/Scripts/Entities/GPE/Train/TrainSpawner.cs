36a27c6f8f6f4081daef19966f0a94e5d9ab881c
Lucas Deutschmann <lucdeutsch@gmail.com>
2025-01-20 08:55:56 +0100

    imported assets

--- Assets/Game/Scripts/Entities/GPE/Train/TrainSpawner.cs
+++ Assets/Game/Scripts/Entities/GPE/Train/TrainSpawner.cs
@@ -1,41 +1,40 @@
 using LemonInc.Core.Utilities;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Game.Entities.GPE.Train
 {
     public class TrainSpawner : MonoBehaviour
     {
-        [SerializeField] float _trainSpeed = 1.0f;
-        [SerializeField] Rigidbody _rb;
-        bool _isTriggered;
-        [SerializeField] Vector3 _StartPos;
-        [SerializeField] Transform _Train;
-        Timer _timer = new();
+        [SerializeField] private float _trainSpeed = 1.0f;
+        [SerializeField] private Rigidbody _rb;
+        private bool _isTriggered;
+        [SerializeField] private Vector3 _startPos;
+        [SerializeField] private Transform _train;
+        private readonly Timer _timer = new();
 
         // Update is called once per frame
         private void Start()
         {
-            _StartPos = _Train.position;
+            _startPos = _train.position;
         }
         private void OnTriggerEnter(Collider other)
         {
-            if (other.tag == "NPC")
-            {
-                _isTriggered = true;
-                _timer.Start(3f, false, ResetPosition);
-            }
-
+            if (!other.CompareTag("NPC")) return;
+            _isTriggered = true;
+            _timer.Start(3f, false, ResetPosition);
         }
         private void FixedUpdate()
         {
             if (_isTriggered)
             {
-                _rb.linearVelocity = transform.forward * _trainSpeed * Time.fixedDeltaTime *10;
+                _rb.linearVelocity = transform.forward * (_trainSpeed * Time.fixedDeltaTime * 10);
             }
         }
-        void ResetPosition()
+
+        private void ResetPosition()
         {
-            _Train.position = _StartPos;
+            _train.position = _startPos;
             _rb.linearVelocity = Vector3.zero;
             _isTriggered = false;
         }
