f2907eeb0d9791078f8ffd2e07fb7d7cd5fc4106
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2025-01-22 12:16:38 +0100

    fix(handicap): ai walk settings

--- Assets/Game/Scripts/Entities/Ai/AiStatData.cs
+++ Assets/Game/Scripts/Entities/Ai/AiStatData.cs
@@ -1,14 +1,12 @@
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace Game.Entities.Ai
 {
     [CreateAssetMenu(menuName = "Data/AiStat")]
     public class AiStatData : ScriptableObject
     {
-        public Vector2 IdleTimeRange;
         public float WalkSpeed;
-        [FormerlySerializedAs("MovementSpeed")] public float RunSpeed;
+        public float RunSpeed;
         public float AttackTriggerDistance;
         public float AttackCooldown;
     }
